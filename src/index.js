ecb77b44a6e23a1b5c0f6437995a6901eeecbb85
CelineCambier <cambier.celine2@gmail.com>
2024-05-05 12:18:23 -0700

    Improved context

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function generateRecipe(event) {
   let apiKey = "e48e1aab0acf4fc215c13f02ct8bo5ab";
   let prompt = `User instructions: Generate a French recipe with ${instructionsInput.value}`;
   let context =
-    "You are a very smart AI and love helping poeple. Your mission is to generate an easy French recipe written in English. Please write the recipe in basic HTML. Make sure to follow the user instructions.";
+    "You are a very smart AI and love helping poeple. Your mission is to generate an easy French recipe written in English. The recipes needs to be provided in HTML Avoid adding ```html at the beginning of the answer. Make sure to follow the user instructions.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let recipeElement = document.querySelector("#recipe");
